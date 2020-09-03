a4b98b9f07018f5cfe8c62d0a823072ce7d1d08a
leandro picoli <leandropicoli@live.com>
2020-09-03 20:36:11 -0300

    Create CustomerRepository

--- LeleStore.Domain/StoreContext/Entities/Address.cs
+++ LeleStore.Domain/StoreContext/Entities/Address.cs
@@ -5,12 +5,13 @@ namespace LeleStore.Domain.StoreContext.Entities
 {
     public class Address : Entity
     {
-        public Address(string street, string number, string complement, string city, string state, string country, string zipCode, EAddressType type)
+        public Address(string street, string number, string complement, string city, string district, string state, string country, string zipCode, EAddressType type)
         {
             Street = street;
             Number = number;
             Complement = complement;
             City = city;
+            District = district;
             State = state;
             Country = country;
             ZipCode = zipCode;
@@ -21,6 +22,7 @@ namespace LeleStore.Domain.StoreContext.Entities
         public string Number { get; private set; }
         public string Complement { get; private set; }
         public string City { get; private set; }
+        public string District { get; private set; }
         public string State { get; private set; }
         public string Country { get; private set; }
         public string ZipCode { get; private set; }
---
+++ LeleStore.Infra/Repositories/CustomerRepository.cs
@@ -0,0 +1,71 @@
+using System.Data;
+using System.Linq;
+using Dapper;
+using LeleStore.Domain.StoreContext.Entities;
+using LeleStore.Domain.StoreContext.Repositories;
+using LeleStore.Infra.StoreContext.DataContexts;
+
+namespace LeleStore.Infra.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly LeleDataContext _context;
+
+        public CustomerRepository(LeleDataContext context)
+        {
+            _context = context;
+        }
+        public bool CheckDocument(string document)
+        {
+            return _context
+                .Connection
+                .Query<bool>(
+                    "spCheckDocument",
+                    new { Document = document },
+                    commandType: CommandType.StoredProcedure)
+                .FirstOrDefault();
+        }
+
+        public bool CheckEmail(string email)
+        {
+            return _context
+                .Connection
+                .Query<bool>(
+                    "spCheckEmail",
+                    new { Email = email },
+                    commandType: CommandType.StoredProcedure)
+                .FirstOrDefault();
+        }
+
+        public void Save(Customer customer)
+        {
+            _context.Connection.Execute("spCreateCustomer",
+            new
+            {
+                Id = customer.Id,
+                FirstName = customer.Name.FirstName,
+                LastName = customer.Name.LastName,
+                Document = customer.Document.Number,
+                Email = customer.Email.Address,
+                Phone = customer.Phone
+            }, commandType: CommandType.StoredProcedure);
+
+            foreach (var address in customer.Addresses)
+            {
+                _context.Connection.Execute("spCreateAddress", new
+                {
+                    Id = address.Id,
+                    CustomerId = customer.Id,
+                    Number = address.Number,
+                    Complement = address.Complement,
+                    District = address.District,
+                    City = address.City,
+                    State = address.State,
+                    Country = address.Country,
+                    ZipCode = address.ZipCode,
+                    Type = address.Type
+                }, commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
